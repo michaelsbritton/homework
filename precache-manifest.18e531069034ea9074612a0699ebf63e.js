0607d5e06acf21f2c370d03f17e6d6c58b453d1d
michaelsbritton <msbritton@gmail.com>
2023-05-25 16:06:22 +0900

    Updates

---
+++ precache-manifest.18e531069034ea9074612a0699ebf63e.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6215a97f457e31e5fea",
+    "url": "/homework/static/css/main.7321bb8e.chunk.css"
+  },
+  {
+    "revision": "b6215a97f457e31e5fea",
+    "url": "/homework/static/js/main.b6215a97.chunk.js"
+  },
+  {
+    "revision": "9bcdb338c956eb72db25",
+    "url": "/homework/static/js/1.9bcdb338.chunk.js"
+  },
+  {
+    "revision": "268c05e3594c5ae2e93a",
+    "url": "/homework/static/css/2.0c37dff5.chunk.css"
+  },
+  {
+    "revision": "268c05e3594c5ae2e93a",
+    "url": "/homework/static/js/2.268c05e3.chunk.js"
+  },
+  {
+    "revision": "5e79509d8d9dfe4d9a45",
+    "url": "/homework/static/js/runtime~main.5e79509d.js"
+  },
+  {
+    "revision": "46235c367347d9814c03ae0ab24a81a2",
+    "url": "/homework/static/media/Primitive.46235c36.ttf"
+  },
+  {
+    "revision": "5a5a58c9fe5fa22e12ff920c74d6d927",
+    "url": "/homework/static/media/abstain from.5a5a58c9.mp3"
+  },
+  {
+    "revision": "209c59c1eee8810d98c406353dfa9ab7",
+    "url": "/homework/static/media/centralize.209c59c1.mp3"
+  },
+  {
+    "revision": "39420c904308f296d6bcb2f81b612987",
+    "url": "/homework/static/media/criticize.39420c90.mp3"
+  },
+  {
+    "revision": "4426449e531abc68c4a7d391ec0d1663",
+    "url": "/homework/static/media/deviate from.4426449e.mp3"
+  },
+  {
+    "revision": "2a5b5524ee304df7d686a666325708bb",
+    "url": "/homework/static/media/enforce.2a5b5524.mp3"
+  },
+  {
+    "revision": "8b71497c4ee1fbe7684534f3339b2fbe",
+    "url": "/homework/static/media/ensure.8b71497c.mp3"
+  },
+  {
+    "revision": "1647acecbe4744dcf3fe728302e69bd6",
+    "url": "/homework/static/media/entitle.1647acec.mp3"
+  },
+  {
+    "revision": "27c9c1e8aded9969c2c10caac29b7826",
+    "url": "/homework/static/media/entrust.27c9c1e8.mp3"
+  },
+  {
+    "revision": "d352d648d1f91351a7d622fdf4df8490",
+    "url": "/homework/static/media/fill in for.d352d648.mp3"
+  },
+  {
+    "revision": "bb32e286046847e3909aad3f54a92658",
+    "url": "/homework/static/media/get through.bb32e286.mp3"
+  },
+  {
+    "revision": "a3e94e1df3b10525cd59ae4034b12352",
+    "url": "/homework/static/media/immunize.a3e94e1d.mp3"
+  },
+  {
+    "revision": "ddedbbf2a61721bb63f0cae77c70689b",
+    "url": "/homework/static/media/minimize.ddedbbf2.mp3"
+  },
+  {
+    "revision": "7f5be16b94650f8fe3a17a9c72439122",
+    "url": "/homework/static/media/mobilize.7f5be16b.mp3"
+  },
+  {
+    "revision": "e9b1bb3ef70358def66dce3d67b9b6fb",
+    "url": "/homework/static/media/settle for.e9b1bb3e.mp3"
+  },
+  {
+    "revision": "e1b0362a1a9daad888a32ea7de69fe4d",
+    "url": "/homework/static/media/sift through.e1b0362a.mp3"
+  },
+  {
+    "revision": "1b7f655f8f3fd959b6a63b7fb52b3982",
+    "url": "/homework/static/media/vandalize.1b7f655f.mp3"
+  },
+  {
+    "revision": "d605a75e75feef3499d59fc158c05c2d",
+    "url": "/homework/static/media/logoMB.d605a75e.svg"
+  },
+  {
+    "revision": "3224d4f260154a2c93938e07584c8dc7",
+    "url": "/homework/index.html"
+  }
+];
