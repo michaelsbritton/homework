a1ddd787de6f06957087b472e9b55234a50373ef
michaelsbritton <msbritton@gmail.com>
2024-01-01 04:37:31 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/homework/precache-manifest.e5ca102c3c38579b8058794310cf4fbb.js"
+  "/homework/precache-manifest.0040c5347b88aef548471ea983fbca08.js"
 );
 
 workbox.clientsClaim();
